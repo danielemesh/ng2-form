871bd99a0828051256658db591d9f9209020d06b
daniel <danielemesh@gmail.com>
2017-06-27 14:20:36 +0300

    Separated error messages into a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { ErrorMessagesComponent } from './form/error-messages/error-messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    ErrorMessagesComponent
   ],
   imports: [
     BrowserModule,
